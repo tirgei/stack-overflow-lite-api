26c74cee65d3164e19e2c998b3fe25cc5607d5f5
Vincent Tirgei <tirgeic@gmail.com>
2018-12-17 01:52:58 +0300

    [Start #162689355] Created the base model

---
+++ app/api/utils/base_model.py
@@ -0,0 +1,51 @@
+
+class Model(object):
+    ''' Represents the base model that all models extend from '''
+
+    def __init__(self, collection_list):
+        ''' initialises the list that holds items of the given model '''
+
+        self.collection = collection_list
+
+    def all(self):
+        ''' Returns all items in the collection list '''
+
+        return self.collection
+
+    def save(self, data):
+        ''' Saves the new model to the list of items '''
+        
+        data['id'] = self.__generate__id()
+        self.collection.append(data)
+
+    def where(self, key, value):
+        '''
+        :param key - key to check
+        :param value - value to check for given key
+        
+        returns list of item found with value of given key '''
+
+        self.query = []
+        
+        for item in self.collection:
+            if item[key] == value:
+                self.query.append(item)
+
+        return self.query
+
+    def exists(self, key, value):
+        '''
+        :param key - key to check
+        :param value - value to check for given key
+        
+        returns true if atleast 1 item found '''
+        return len(self.where(key, value)) > 0
+
+    def __generate__id(self):
+        ''' Generates ID for the new model '''
+
+        if len(self.collection):
+            return self.collection[-1]['id'] + 1
+        else:
+            return 1
+    
