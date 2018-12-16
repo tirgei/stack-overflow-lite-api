e361ea71e4f1e5d9d93b7c5b6cfc302da46427b9
Vincent Tirgei <tirgeic@gmail.com>
2018-12-17 01:55:01 +0300

    [Feature #162689355] Updated password validator

--- app/api/utils/validators.py
+++ app/api/utils/validators.py
@@ -8,4 +8,20 @@ class Validators():
 
     def valid_password(self, password):
         ''' Check if password is valid '''
-        return re.match(r'[A-Za-z0-9@#$%^&+=]{6,}', password)
+
+        if len(password) < 8:
+            return False
+
+        scores = {}
+
+        for letter in password:
+            if letter.islower():
+                scores['has_lower'] = 1
+
+            if letter.isupper():
+                scores['has_upper'] = 1
+
+            if letter.isdigit():
+                scores['has_digit'] = 1
+
+        return sum(scores.values()) == 3
