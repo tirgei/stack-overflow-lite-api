46d46a5e224975d343346da87235106c73dbc87a
Vincent Tirgei <tirgeic@gmail.com>
2018-12-17 13:13:37 +0300

    [Feature #162696358] Added function to add added attributes to user

--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -14,4 +14,7 @@ class User(Model):
             if key in user:
                 user.update({key: value})
 
+            if not hasattr(user, key):
+                user[key] = value
+            
         return user
