e2c662cb557c3553388a7fe849360d3c03a8870b
Vincent Tirgei <tirgeic@gmail.com>
2018-12-19 18:24:59 +0300

    [Feature  #162759099] Added functions to generate & decode auth token

--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -1,4 +1,7 @@
 from app.api.utils.base_model import Model
+from datetime import datetime, timedelta
+import os
+import jwt
 
 users = [
     {
@@ -29,6 +32,8 @@ class User(Model):
         super().__init__(users)
 
     def update(self, id, data):
+        """ Function to update user details """
+
         user = [u for u in super().all() if id == u['id']][0]
 
         for key, value in data.items():
@@ -37,5 +42,39 @@ class User(Model):
 
             if not hasattr(user, key):
                 user[key] = value
-            
+
+        user['updated_on'] = datetime.now()
         return user
+
+    def generate_token(self, id):
+        """ Function to generate the authentication token """
+
+        try:
+            payload = {
+                'exp' : datetime.utcnow() + timedelta(days=0, seconds=30),
+                'iat' : datetime.utcnow(),
+                'sub' : id
+            }
+
+            return jwt.encode(
+                payload,
+                os.getenv('SECRET_KEY', 'canttouchthis'),
+                algorithm='HS256'
+            )
+
+        except Exception as e:
+            return e
+
+        @staticmethod
+        def decode_token(token):
+            """ Decodes the auth token and returns the user ID """
+
+            try:
+                payload = jwt.decode(token, os.getenv('SECRET_KEY'))
+                return payload['sub']
+            
+            except jwt.ExpiredSignatureError:
+                return 'Signature expired. Please log in again'
+
+            except jwt.InvalidTokenError:
+                return 'Invalid token. Please log in again'
