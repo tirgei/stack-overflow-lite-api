15efbc2a657a9f4b182cace90b1f97e65e6a1f28
Vincent Tirgei <tirgeic@gmail.com>
2018-12-19 15:03:13 +0300

    [Finishes  #162733260] Registered Blueprint for users

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 import os
 from flask import Flask
 from instance.config import app_config
-from app.api.v1.views.user_views import auth, users
+from app.api.v1.views.user_views import v1 as users_v1
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
 
-    app.register_blueprint(auth)
-    app.register_blueprint(users)
+    app.register_blueprint(users_v1)
 
     return app
