9c9e75bbdf5f0ff8d461013a97c57e71e601fa3b
Vincent Tirgei <tirgeic@gmail.com>
2018-12-20 18:35:47 +0300

    [Feature #162781098] Updated base test class

--- app/tests/v1/base_test.py
+++ app/tests/v1/base_test.py
@@ -1,4 +1,20 @@
 import unittest
 import json
-import os
 from app import create_app
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        """ Initialize test variables """
+
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        """ Destroy the test client when done """
+        
+        self.app.testing = False
+        self.app = None
+    
+    if __name__ == '__main__':
+        unittest.main()
